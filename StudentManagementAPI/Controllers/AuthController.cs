64db7afddd3e1e7b293edd6ee6b7007d344abfd1
Sadashiv Bobhate <sadashiv@logitax.in>
2026-04-07 11:42:50 +0530

    Fix: DTO_s to DTOs

--- StudentManagementAPI/Controllers/AuthController.cs
+++ StudentManagementAPI/Controllers/AuthController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using StudentManagementAPI.DTO_s;
+using StudentManagementAPI.DTOs;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
